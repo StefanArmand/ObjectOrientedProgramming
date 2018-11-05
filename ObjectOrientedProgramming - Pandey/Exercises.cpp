4a2f26aade4cbfd46043130c36a713d2f561dd88
Stefan <stefan_armand@yahoo.com>
2018-11-05 16:13:16 +0200

    Created empty for feature branch

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,24 +1,8 @@
-// Programming exercises
-// Write a program to enter a four digit number and find out sum of its digit using % and / operator
-
 #include "pch.h"
 #include <iostream>
 
 using namespace std;
 int main()
 {
-	int num = 0, r;
-	int sum = 0;
-
-	cout << "Enter a four digit number \n";
-	cin >> num;
-
-		sum = 0;
-		while (num!=0) {
-			r = num % 10;
-			sum = sum + r;
-			num = num / 10;
-		}
-
-		cout << "Sum of digits is : " << sum << endl;
+	
 }
