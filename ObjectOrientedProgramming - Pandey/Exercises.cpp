0d65bac938bb4c55ae95efaf8eb5f620ef190f77
Stefan <stefan_armand@yahoo.com>
2018-11-05 15:52:07 +0200

    a program to enter a four digit number and find out sum of its digit using % and / operator

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,6 +1,5 @@
 // Programming exercises
-// Write a program to convert entered temperature from celsius to fahrenheit
-// Formula is (0°C × 9 / 5) + 32 = 32°F
+// Write a program to enter a four digit number and find out sum of its digit using % and / operator
 
 #include "pch.h"
 #include <iostream>
@@ -8,10 +7,18 @@
 using namespace std;
 int main()
 {
-	float c = 0;
-	float fahr = 0;
-	cout << "Enter the temperature in Celsius: \n";
-	cin >> c;
-	fahr = (c * 9 / 5) + 32;
-	cout << "The temperature in Fahrenheit is: " << fahr << endl;
+	int num = 0, r;
+	int sum = 0;
+
+	cout << "Enter a four digit number \n";
+	cin >> num;
+
+		sum = 0;
+		while (num!=0) {
+			r = num % 10;
+			sum = sum + r;
+			num = num / 10;
+		}
+
+		cout << "Sum of digits is : " << sum << endl;
 }
