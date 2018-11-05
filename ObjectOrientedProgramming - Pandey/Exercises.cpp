c2d29149be1504d8e2e86ad59d644da611b35960
Stefan <stefan_armand@yahoo.com>
2018-11-05 17:05:43 +0200

    program to display number all alphabets (upper and lowew) along with their ASCII values

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,8 +1,15 @@
+//Write a program to display number all alphabets (upper and lowew) along with their ASCII values
+
 #include "pch.h"
 #include <iostream>
 
 using namespace std;
 int main()
 {
-	
+	char c = 'a';
+	char c2 = 'z';
+	while (c <= c2) {
+		cout << "For character: " << c << " the ASCII is " << int(c) << endl;
+		c++;
+	}
 }
