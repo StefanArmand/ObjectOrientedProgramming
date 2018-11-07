4f1ea33f683092887e1fe67e432ac8b4957d99a3
Stefan <stefan_armand@yahoo.com>
2018-11-07 12:04:39 +0200

    program that will read the values of x and evaluate the following function 1 for x > 0 y = 0 for x = 0 -1 for x < 0

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,51 +1,28 @@
-//Write a program to find out largest of 10 numbers using if and goto only
+//Write a program that will read the values of x and evaluate the following function
+// 1 for x > 0
+// y = 0 for x = 0
+// -1 for x < 0
+
 #include "pch.h"
 #include <iostream>
 
 using namespace std;
 int main()
 {
-	int n[10] = {32, 42, 32, 12, 32, 21, 11, 27, 32, 42};
-	int largest = 0;
-	bool first = true;
+	int x = 0;
+	int result = 0;
+	cout << "Write the x value: \n";
+	cin >> x;
 
-	/*cout << "Enter 10 numbers: \n";
-	for (int i = 0; i < 10; i++) {
-		cin >> n[i];
-	}*/
-
-	int c = 0;
-	while (c < 10) {
-		if (first) {
-			first = false;
-			c++;
-			continue;
-		}
-		if (largest != 0) {
-			goto complargest;
-		}
-		if (n[c + 1] > n[c]) {
-			largest = n[c + 1];
-			c++;
-			continue;
-		}
-		else {
-			largest = n[c];
-			c++;
-			continue;
-		}
-
-	complargest:
-		if (largest > n[c]) {
-			c++;
-			continue;
-		}
-		else {
-			largest = n[c];
-			c++;
-			continue;
-		}
+	if (x < 0) {
+		result = -1;
+	}
+	else if (x == 0) {
+		result = 0;
+	}
+	else if (x > 0){
+		result = 1;
 	}
 
-	cout << "Largest number is: " << largest << endl;
+	cout << "The result is " << result << endl;
 }
