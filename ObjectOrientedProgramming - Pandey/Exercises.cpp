c5ef5f0cc992074f7548994a74e2c8acbf130c8c
Stefan <stefan_armand@yahoo.com>
2018-11-07 13:58:15 +0200

    program to calculate the sum of remainders obtained by dividing with modular division operations by 2 on 1 to 9 numbers

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,7 +1,4 @@
-//Write a program to calculate the bill of Internet Browsing. The conditions are given below:
-// 1 hour - 20$
-// 1/2 hours - 15$
-// 5 hours - 90$
+//Write a program to calculate the sum of remainders obtained by dividing with modular division operations by 2 on 1 to 9 numbers
 
 #include "pch.h"
 #include <iostream>
@@ -9,20 +6,16 @@
 using namespace std;
 int main()
 {
-	float n = 0;
-	float totaltopay = 0;
-	cout << "Write the number of hours: \n";
-	cin >> n;
+	int n = 0;
+	int sum = 0;
+	//cout << "Write the number of hours: \n";
+	//cin >> n;
 
-	if (n <= 1.0f && n > 0.5f || n > 0.5f && n < 5.0f) {
-		totaltopay = 20 * n;
-	}
-	else if (n <= 0.5f) {
-		totaltopay = 30 * n;
-	}
-	else if (n == 5) {
-		totaltopay = 90;
+	for (int i = 1; i < 10; i++) {
+		int tmp = 0;
+		tmp = i % 2;
+		sum += tmp;
 	}
 
-	cout << "The total to pay is " << totaltopay << endl;
+	cout << "The total is " << sum << endl;
 }
