a499e22606182644f65ebe7f3957666390264193
Stefan <stefan_armand@yahoo.com>
2018-11-07 12:10:53 +0200

    a program to calculate the amount of the bill for the following jobs a. Scanning and hardcopy of a passport photo 5$ b. Scanning and hardcopies (more than 10) 3$

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,7 +1,6 @@
-//Write a program that will read the values of x and evaluate the following function
-// 1 for x > 0
-// y = 0 for x = 0
-// -1 for x < 0
+//Write a program to calculate the amount of the bill for the following jobs
+// a. Scanning and hardcopy of a passport photo 5$
+// b. Scanning and hardcopies (more than 10) 3$
 
 #include "pch.h"
 #include <iostream>
@@ -9,20 +8,20 @@
 using namespace std;
 int main()
 {
-	int x = 0;
-	int result = 0;
-	cout << "Write the x value: \n";
-	cin >> x;
+	int n = 0;
+	int totaltopay = 0;
+	cout << "Write the number of passports: \n";
+	cin >> n;
 
-	if (x < 0) {
-		result = -1;
+	if (n < 10 && n > 0 ) {
+		totaltopay  = n*5;
 	}
-	else if (x == 0) {
-		result = 0;
+	else if (n >= 10) {
+		totaltopay = n*3;
 	}
-	else if (x > 0){
-		result = 1;
+	else if (n <= 0){
+		cout << "n must be positive" << endl;
 	}
 
-	cout << "The result is " << result << endl;
+	cout << "The total to pay is " << totaltopay << endl;
 }
