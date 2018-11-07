23c2db515c2185c27fdb6d2fe32fe6d6957a8e90
Stefan <stefan_armand@yahoo.com>
2018-11-07 10:10:11 +0200

    program to check whether a triangle can be formed with three positive integers supplied

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,23 +1,35 @@
-//Write a program to check whether a voter is eligible for voting or not? If his/her age is equal to or greater than 18 display "Eligible" else display "Not eligible"
+//Write a program to check whether a triangle can be formed with three positive integers supplied
 
 #include "pch.h"
 #include <iostream>
-#include <bitset>
 
 using namespace std;
 int main()
 {
-	int age = 0;
-	
-	cout << "Enter the age of the voter: \n";
-	cin >> age;
+	int s1 = 0;
+	int s2 = 0;
+	int s3 = 0;
 
-	if (age < 18) {
-		cout << "The voter is eligible \n";
+	cout << "Enter the first side: \n";
+	cin >> s1;
+
+	cout << "Enter the second side: \n";
+	cin >> s2;
+
+	cout << "Enter the third side: \n";
+	cin >> s3;
+
+	
+	if (s1 + s2 <= s3) {
+		cout << "The triangle can't be formed \n";
+	}
+	else if (s1 + s3 <= s2) {
+		cout << "The triangle can't be formed \n";
+	}
+	else if (s2 + s3 <= s1) {
+		cout << "The triangle can't be formed \n";
 	}
 	else {
-		cout << "The voter is not eligible \n";
-	}
-
-	
+		cout << "The triangle can be formed \n";
+	}	
 }
