79534e2dd2caedc2919efd48c8c059419cb3ed78
Stefan <stefan_armand@yahoo.com>
2018-11-13 15:15:48 +0200

    Write a program to allocate memory for 10 string using new and display them

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,8 +1,22 @@
+// Write a program to allocate memory for 10 string using new and display them
+
 #include "pch.h"
 #include <iostream>
 
 using namespace std;
-int main()
+void main()
 {
-	
+	int n;
+	int numb = 0;
+	while (numb < 10) {
+		cout << "Enter the length of the string number " << numb << "\n";
+		cin >> n;
+		char *str = new char[n + 1];
+		cout << "Enter the string\n";
+		cin >> str;
+		cout << "Hello " << str << endl;
+		delete str;
+		numb++;
+	}
+
 }
