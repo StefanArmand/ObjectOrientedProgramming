b7e39c3b6488c48e90bb543d4b9c7b2080e3ea80
Stefan <stefan_armand@yahoo.com>
2018-11-22 19:45:58 +0200

    Write a program to list all the strings whose initials starts with a given input character using function and pointer

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,36 +1,24 @@
-//Write a program to accept a float number through the keyboard. Calculate the square of the
-//number.Separate the float number into integer and fractional part.Individually calculate the
-//square of an integer and fractional part and add them in another variable.Compare the two
-//squares obtained.
+//Write a program to list all the strings whose initials starts with a given input character using function and pointer
 
 #include "pch.h"
 #include <iostream>
-#include <cmath>
+#include <string>
 
 using namespace std;
 
+
 int main()
 {
-	float fl = 0.0f;
-	float sqfl = 0.0f;
-	double sqfract = 0;
-	int sqint = 0;
-	int intfl = 0;
-	double fractpart;
-	double intpart;
-	double sqtotal = 0;
+	const char* strlist[4] = { "first", "frist", "jimcond", "third" };
+	char c = ' ';
+	cout << "Enter the character to be compared: " << endl;
+	cin >> c;
 
-	cout << "Please enter a float: " << endl;
-	cin >> fl;
-
-	sqfl = fl * fl;
-
-	fractpart = modf(fl, &intpart);
-
-	sqfract = fractpart * fractpart;
-	sqint = intpart * intpart;
-	sqtotal = sqfract + sqint;
-
-
-	cout << "The square of the float is: " << sqfl << " and the square of the fractpart + intpart is: " << sqtotal << endl;
+	for (int i = 0; i < 4; i++) {
+		
+		char firstlet = strlist[i][0];
+		if (c == firstlet) {
+			cout << strlist[i] << endl;
+		}
+	}
 }
