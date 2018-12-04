9aad1320834389af2b9b35a7270406a382609b6e
Stefan <stefan_armand@yahoo.com>
2018-12-04 17:53:02 +0200

    Define a class fract whose object represent rational numbers (i.e., fractions). Inside integer data members numr and denr for storing a numerator and a denominator respectively. Provide a constructor and member function eval_fract() for evaluating the value of the rational number, invert() for inverting the rational number, print() for printing the rational number in the form numr / denr(for example, 22 / 7).Also provide access functions get_numr and get_denr for returning the values of the private data members.Include a member function reduce_fract(), that reduces the fraction numr / denr to lowest term.For example, the fraction 54 / 90 is stored as the object 3 / 5. Invoke these member functions in the main() module to test them with suitable data.

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,5 +1,7 @@
-// Write a function that takes two Distance (structure) value as arguments and returns the larger one.
-//Include a main program that accepts two Distance values from the user, compare them and display the larger.
+// Define a class fract whose object represent rational numbers (i.e., fractions). Inside integer data members numr and denr for storing a numerator and a denominator respectively.Provide a
+// constructor and member function eval_fract() for evaluating the value of the rational number, invert() for inverting the rational number, print() for printing the rational number in the form
+// numr / denr(for example, 22 / 7).Also provide access functions get_numr and get_denr for returning the values of the private data members.Include a member function reduce_fract(), that reduces
+// the fraction numr / denr to lowest term.For example, the fraction 54 / 90 is stored as the object 3 / 5. Invoke these member functions in the main() module to test them with suitable data.
 
 #include "pch.h"
 #include <iostream>
@@ -7,32 +9,83 @@
 
 using namespace std;
 
-struct Distance
+class fract
 {
-	int dist1 = 0;
-	int dist2 = 0;
 
-	void input_data(int d1, int d2) {
-		dist1 = d1;
-		dist2 = d2;
+	int numr = 0; // numerator
+	int denr = 0; // denominator
+
+public:
+
+	void get_numr(int n) {
+		numr = n;
 	}
 
-	int compare() {
-		if (dist1 > dist2) {
-			return dist1;
-		}
-		else {
-			return dist2;
-		}
+	void get_denr(int d) {
+		denr = d;
 	}
+
+	float eval_fract() {
+		float x = (float)numr / (float)denr;
+		return x;
+	}
+
+	void invert() {
+		int temp = denr;
+		denr = numr;
+		numr = temp;
+
+		cout << "The inverted is: " << numr << " / " << denr << endl;
+	}
+
+	void print() {
+		cout << " The fractal is: : " << numr << " / " << denr << endl;
+	}
+
+	void reduce_fract() {
+		int numrt = numr;
+		int denrt = denr;
+		while (1) {
+			if (numrt % 2 == 0 && denrt % 2 == 0) {
+				numrt = numrt / 2;
+				denrt = denrt / 2;
+				continue;
+			}
+			else if (numrt % 3 == 0 && denrt % 3 == 0) {
+				numrt = numrt / 3;
+				denrt = denrt / 3;
+				continue;
+			}
+			else if (numrt % 5 == 0 && denrt % 5 == 0) {
+				numrt = numrt / 5;
+				denrt = denrt / 5;
+				continue;
+			}
+			else if (numrt % 7 == 0 && denrt % 7 == 0) {
+				numrt = numrt / 7;
+				denrt = denrt / 7;
+				continue;
+			}
+			else {
+				break;
+			}
+		}
+		cout << " The fractal to the lowest term is: : " << numrt << " / " << denrt << endl;
+	}
+
 };
 
 void main()
 {
-	Distance d;
-	d.input_data(35, 45);
+	fract f;
+	f.get_numr(36);
+	f.get_denr(68);
 
-	cout << "The distances are: " << d.dist1 << " and " << d.dist2 << endl;
+	f.print();
+	cout << "The fractal evaluated is: " << f.eval_fract() << endl;
+	f.invert();
 
-	cout << "The bigger distance is: " << d.compare() << endl;
+	f.reduce_fract();
+
+
 }
