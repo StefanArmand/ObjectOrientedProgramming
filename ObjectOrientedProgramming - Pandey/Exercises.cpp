bd8488394eb02f8ab3656c93f3443257e00ce1d0
Stefan <stefan_armand@yahoo.com>
2018-12-05 14:04:54 +0200

    Create a class time that has separate int member data for hours, minutes and seconds. One constructor should initialize this data to 0, and another should initialize it to fixed values.Another member function should display it, in 11:59 : 59 format.The final member function should add two objects of type time passed as arguments.  A main program should create two initialized time objects(should they be const ? ) and one that is not initialized.Then it should add the two initialized values together, leaving the result in the third time variable.Finally, it should display the value of this third variable.Make appropriate member functions const.

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,35 +1,62 @@
-// Create a class that includes a data member that holds a “serial number” for each object created from the class.That is, the first object created will be numbered 1, the second 2 and so on.
-// To do this, you will need another data member that records a count of how many object have been created so far. (This member should apply to the class as a whole; not to individual objects.What
-//	keyword specifies this ? ) Then, as each object is created, its constructor can examine this count 	member variable to determine the appropriate serial number for the new object.
-//	Add a member function that permits an object to report its own serial number.Then write a main () program that creates three objects and quires each one about its serial number.They should
-//	respond I am object number 2, and so on.
+// Create a class time that has separate int member data for hours, minutes and seconds. One constructor should initialize this data to 0, and another should initialize it to fixed values.Another
+// member function should display it, in 11:59 : 59 format.The final member function should add two objects of type time passed as arguments.
+// A main program should create two initialized time objects(should they be const ? ) and one that is not initialized.Then it should add the two initialized values together, leaving the result in the third time variable.Finally, it should display the value of this third variable.Make appropriate
+// member functions const.
 
 #include "pch.h"
 #include <iostream>
 
 using namespace std;
 
-class Serial
+class Time
 {
 
-	static int serNumb; 
+	int hours;
+	int minutes;
+	int secs;
 
 
 public:
 
-	void called() {
-		serNumb++;
-		cout << "I am object number " << serNumb << endl;
+	void initializetozero() {
+		hours = 0;
+		minutes = 0;
+		secs = 0;
+	}
+	void input_time(int hh, int mm, int ss)
+	{
+		hours = hh;
+		minutes = mm;
+		secs = ss;
 	}
 	
-};
+	void show_time(Time t)
+	{
+		cout << t.hours << " : " << t.minutes
+			<< "\t" << " : " << t.secs << endl;
+	}
 
-int Serial::serNumb;
+
+	Time sum_time(Time A, Time B)
+	{
+		int h, m, s;
+		Time temp;
+		s = A.secs + B.secs;
+		m = A.minutes + B.minutes + s / 60;
+		h = A.hours + B.hours + m / 60;
+		temp.secs = s % 60;
+		temp.minutes = m % 60;
+		temp.hours = h;
+		return temp;
+	}
+};
 
 int main()
 {
-	Serial s1, s2, s3;
-	s1.called();
-	s2.called();
-	s3.called();
+	Time t1, t2, t3;
+	t1.initializetozero();
+	t2.input_time(13, 45, 22);
+	t3.input_time(5, 21, 11);
+	t1.sum_time(t2, t3);
+	t1.show_time(t1.sum_time(t2, t3));
 }
