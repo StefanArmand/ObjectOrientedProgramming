24cf4202e6cd5c63a4be459475a6b7a4cf8b0137
Stefan <stefan_armand@yahoo.com>
2018-12-09 14:10:16 +0200

    Define a class named Queue for holding ints and providing function for inserting items at one end and removing items from other end of a queue data structure to be supported by this class.Include a default constructor, a destructor and the usual queue operations : insert(), remove(), isempty (), and isfull().Use array implementation.

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,43 +1,79 @@
-//Define a class called time that has separate int data members for hours, minutes and seconds.One constructor should initialize these data to 0 and another should initialize them to fixed values.
-//Write a member function to display time in the format 12 :28 : 34. Another member function should add two objects of type time passed as arguments.
+﻿//Define a class named Queue for holding ints and providing function for inserting items at one end and removing items from other end of a queue data structure to be supported by this class.Include
+// a default constructor, a destructor and the usual queue operations : insert(), remove(), isempty (), and isfull().Use array implementation.
 
 #include "pch.h"
 #include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+
+#define S 5
 
 using namespace std;
 
-class time {
-	int sec, min, hours;
-
-public:
-	time() {
-		sec = min = hours = 0;
-	}
-	time(int s, int m, int h) {
-		sec = s;
-		min = m;
-		hours = h;
-	}
-
-	void show() {
-		cout << "The times is: " << hours << " : " << min << " : " << sec << endl;
-	}
-
-	friend time addtime(time, time);
-};
-
-time addtime(time A, time B) {
-	time temp;
-	temp.hours = A.hours + B.hours;
-	temp.min = A.min + B.min;
-	temp.sec = A.sec + B.sec;
-	return temp;
-}
-
-using namespace std;
-int main()
+class queue
 {
-	time d1(31, 15, 3);
-	time d2(15, 10, 5);
-	addtime(d1, d2).show();
+	int front;
+	int rear;
+	int que[S];
+public:
+	queue()
+	{
+		front = rear = -1;
+	}
+	void insert(int item)
+	{
+		if (rear == S-1)
+		{
+			cout << "QUEUE IS FULL" << endl;
+			exit(0);
+		}
+		if (front == -1)
+			front = 0;
+		que[++rear] = item;
+		cout << "ITEM INSERTED" << endl;
+	}
+	int remove()
+	{
+		int item;
+		if (front == -1)
+		{
+			cout << "QUEUE IS EMPTY " << endl;
+			exit(0);
+		}
+		item = que[front];
+		if (front == rear)
+			front = rear = -1;
+		else
+			front++;
+		return item;
+	}
+	~queue() {
+	}
+};
+void main()
+{
+	queue s;
+	int choice, item;
+	do
+	{
+		cout << "QUEUW DEMO" << endl;
+		cout << "1 : INSERT(REAR OPERATION)" << endl;
+		cout << "2 : DELETE(FRONT OPERATION)" << endl;
+		cout << "3 : QUIT" << endl;
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:cout << "ENTER THE ITEM" << endl;
+			cin >> item;
+			s.insert(item);
+			break;
+		case 2: item = s.remove();
+			cout << "ITEM DELETED : = " << item << endl;
+			break;
+		case 3: cout << "HAVE A NICE DAY, GOOD BYE" << endl;
+			exit(0);
+			break;
+		default: cout << "WRONG CHOICE!ERROR......." << endl;
+		}
+	} while (choice >= 1 && choice <= 3);
 }
