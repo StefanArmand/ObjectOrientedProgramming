8904348fcbf0bf2ebb1fb165c7b678ee45627753
Stefan <stefan_armand@yahoo.com>
2018-12-09 14:24:19 +0200

    Define a class of square matrix N ×N of integers.Define the necessary constructor / destructor and other members.Write a program to find the trace(sum of diagonal elements) of a matrix.

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,79 +1,69 @@
-﻿//Define a class named Queue for holding ints and providing function for inserting items at one end and removing items from other end of a queue data structure to be supported by this class.Include
-// a default constructor, a destructor and the usual queue operations : insert(), remove(), isempty (), and isfull().Use array implementation.
+﻿// Define a class of square matrix N ×N of integers.Define the necessary constructor / destructor and other members.Write a program to find the trace(sum of diagonal elements) of a matrix.
 
 #include "pch.h"
 #include <iostream>
 #include <conio.h>
 #include <stdlib.h>
 
-#define S 5
-
 using namespace std;
 
-class queue
+class matrix
 {
-	int front;
-	int rear;
-	int que[S];
+	int **ptr;
+	int row, col;
 public:
-	queue()
-	{
-		front = rear = -1;
-	}
-	void insert(int item)
-	{
-		if (rear == S-1)
-		{
-			cout << "QUEUE IS FULL" << endl;
-			exit(0);
-		}
-		if (front == -1)
-			front = 0;
-		que[++rear] = item;
-		cout << "ITEM INSERTED" << endl;
-	}
-	int remove()
-	{
-		int item;
-		if (front == -1)
-		{
-			cout << "QUEUE IS EMPTY " << endl;
-			exit(0);
-		}
-		item = que[front];
-		if (front == rear)
-			front = rear = -1;
-		else
-			front++;
-		return item;
-	}
-	~queue() {
-	}
+	matrix(int r, int c);
+	void input();
+	void show();
+	int diag_sum(int r, int c);
 };
-void main()
+
+matrix :: matrix(int r, int c)
 {
-	queue s;
-	int choice, item;
-	do
-	{
-		cout << "QUEUW DEMO" << endl;
-		cout << "1 : INSERT(REAR OPERATION)" << endl;
-		cout << "2 : DELETE(FRONT OPERATION)" << endl;
-		cout << "3 : QUIT" << endl;
-		cin >> choice;
-		switch (choice)
-		{
-		case 1:cout << "ENTER THE ITEM" << endl;
-			cin >> item;
-			s.insert(item);
-			break;
-		case 2: item = s.remove();
-			cout << "ITEM DELETED : = " << item << endl;
-			break;
-		case 3: cout << "HAVE A NICE DAY, GOOD BYE" << endl;
-			exit(0);
-			break;
-		default: cout << "WRONG CHOICE!ERROR......." << endl;
-		}
-	} while (choice >= 1 && choice <= 3);
+	row = r; col = c;
+	ptr = new int *[row];
+	for (int i = 0; i < row; i++)
+		ptr[i] = new int[col];
 }
+void matrix :: input()
+	{
+		int i, j;
+		for (i = 0; i < row; i++)
+			for (j = 0; j < col; j++)
+			{
+				cout << "\nEnter ptr[" << i << "][" << j << "] element :";
+				cin >> ptr[i][j];
+			}
+		}
+void matrix :: show()
+	{
+		int i, j;
+		cout << "\n Matrix is \n";
+		for (i = 0; i < row; i++)
+		{
+			for (j = 0; j < col; j++)
+				cout << ptr[i][j] << "\t";
+			cout << endl;
+		}
+	}
+int matrix :: diag_sum(int r, int c)
+{
+	r = row;
+	c = col;
+	int i, j, sum = 0;
+	for (i = 0; i < r; i++)
+	{
+		for (j = 0; j < c; j++)
+			if (i == j)
+				sum += ptr[i][j];
+	}
+	return sum;
+}
+
+void main()
+	{
+		matrix m(3, 3);
+		m.input();
+		m.show();
+		cout << "The sum of the diagonal is: " << m.diag_sum(3, 3) << endl;
+	}
