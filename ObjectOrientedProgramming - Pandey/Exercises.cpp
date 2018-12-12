09a08464611fb341807f9830701d025cdb3e513c
Stefan <stefan_armand@yahoo.com>
2018-12-12 16:38:48 +0200

    Write a program that substitutes an overloaded += operator for the overloaded + operator. This operator should allow statements like : s1 + = s2;  Where s2 is added(concatenated) to s1 and the result is left in s1.The operator should also permits the results of the operation to be used in other calculations, as in s3 = s1 + = s2;

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,43 +1,43 @@
-// Write a program to declare two classes Eur and Dollar. Declare objects of both the classes and perform conversion between Rupees and Dollar using any of the conversion method.
+// Write a program that substitutes an overloaded += operator for the overloaded + operator. This operator should allow statements like : s1 + = s2; 
+// Where s2 is added(concatenated) to s1 and the result is left in s1.The operator should also permits the results of the operation to be used in other calculations, as in s3 = s1 + = s2;
 #include "pch.h"
 #include <iostream>
+#include <math.h>
 
 using namespace std;
-class Eur
-{
-	double sum;
-public:
-	Eur(int x)
-	{
-		sum = x* 1.13;
-		show();
-	}
-
-	void show()
-	{
-		cout << "Eur to dollars = " << sum << endl;
-	}
-};
-class USD
-{
-	double sum;
-public:
-	USD(int x)
-	{
-		sum = x * 0.88;
-		show();
-	}
-
-	void show()
-	{
-		cout << "USD to euro = " << sum << endl;
-	}
-};
-int main()
+class PlusEquals
 {
 	int num;
-	cout << "Enter the num" << endl;
-	cin >> num;
-	Eur d = num;
-	USD d1 = num;
+	static int count;
+public:
+
+	PlusEquals operator +(PlusEquals x) {
+		PlusEquals t;
+		t.num = num + x.num;
+		return t;
+	}
+
+	void show()
+	{
+		cout << "The sum is = " << num << endl;
+	}
+
+	void input()
+	{
+		cout << " Enter the number for object" << ++count << endl;
+		cin >> num;
+	}
+
+};
+
+int PlusEquals::count;
+
+int main()
+{
+	PlusEquals n1, n2, n3, n4;
+	n1.input();
+	n2.input();
+	n3.input();
+	n4 = n1 + n2 + n3;
+	n4.show();
 }
