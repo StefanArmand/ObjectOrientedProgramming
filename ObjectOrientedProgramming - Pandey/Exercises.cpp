f1c3b3a57e0bd681db32c5eb2a1f9fffbc4c1bc2
Stefan <stefan_armand@yahoo.com>
2018-12-12 14:34:09 +0200

    Write C++ program to overload a function named power() to allow the calculation of power of both ‘int’ and ‘float’ value.

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,8 +1,33 @@
+// Write C++ program to overload a function named power() to allow the calculation of power of both ‘int’ and ‘float’ value.
+
 #include "pch.h"
 #include <iostream>
 
 using namespace std;
-int main()
+class power
 {
-	
+	int pow;
+public:
+	power(int x)
+	{
+		pow = x*x;
+		show();
+	}
+
+	power(float x) {
+		pow = x * x;
+		show();
+	}
+
+	void show()
+	{
+		cout << "squared = " << pow << endl;
+	}
+};
+void main()
+{
+	float num;
+	cout << "Enter the num" << endl;
+	cin >> num;
+	power d = num;
 }
