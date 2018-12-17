a85756eea58f611663d2f8e4228b46b955b4f6d7
Stefan <stefan_armand@yahoo.com>
2018-12-17 17:11:42 +0200

    Imagine a publishing company that markets both book and audiocassette versions of its works. Create a class publication that stores the title (a string) and price (type float) of publication. From this class derive two classes : book, which adds a page count (type int); and tape, which adds a playing time in minutes (type float). Each of these three classes should have a getdata ( ) function to get its data from the user at the keyboard and a putdata ( ) function to display its data. Write a main( ) program to test the book and tape classes by creating instance of them, asking the user to fill in data with getdata( ) and then displaying data with putdata( ).

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,32 +1,71 @@
-//Write a program to declare classes A, B and C. Each class contains one char array as a data member.Apply multiple inheritances.Concatenate strings of class A and B and store it in class C.
+//Imagine a publishing company that markets both book and audiocassette versions of its works. Create a class publication that stores the title(a string) and price(type float) of publication.From
+// this class derive two classes : book, which adds a page count(type int); and tape, which adds a playing time in minutes(type float).Each of these three classes should have a getdata() function
+// to get its data from the user at the keyboard and a putdata() function to display its data. Write a main() program to test the book and tape classes by creating instance of them, asking the
+// user to fill in data with getdata() and then displaying data with putdata().
 
 #include "pch.h"
 #include <iostream>
-#include <string.h>
+#include <string>
+
 using namespace std;
-class A
+class publication
 {
 protected:
-	char astr[40] = "stringfromA";
-};
-class B
-{
-protected:
-	char bstr[20] = "stringfromB";
-};
-class C : public A, public B
-{
+	string title;
+	float price;
 public:
-	void concatanete_string()
-	{
-		strcat_s(astr, bstr);
-		cout << "These are the strings concatenated: " << astr << endl;
+	void getdata() {
+		cout << "Write the title: " << endl;
+		cin >> title;
+		cout << "Write the price: " << endl;
+		cin >> price;
+	}
+
+	void putdata() {
+		cout << "The title is: " << title << "\nThe price is: " << price << endl;
+	}
+
+};
+class book : public publication
+{
+protected:
+	int pagecount;
+public:
+	void getdata() {
+		publication::getdata();
+		cout << "Write the number of pages: " << endl;
+		cin >> pagecount;
+	}
+
+	void putdata() {
+		publication::putdata();
+		cout << "The number of pages is: " << pagecount << endl;
+	}
+};
+class tape : public publication
+{
+protected:
+	float playingtime;
+public:
+	void getdata() {
+		publication::getdata();
+		cout << "Write the playing time in minutes: " << endl;
+		cin >> playingtime;
+	}
+
+	void putdata() {
+		publication::putdata();
+		cout << "The  playing time in minutes is: " << playingtime << endl;
 	}
 };
 
 
 int main()
 {
-	C o1;
-	o1.concatanete_string();
+	book b1;
+	tape t1;
+	b1.getdata();
+	b1.putdata();
+	t1.getdata();
+	t1.putdata();
 }
