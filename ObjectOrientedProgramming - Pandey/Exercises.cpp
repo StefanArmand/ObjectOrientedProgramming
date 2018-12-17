f0406518fe7630178b724175d4224e831b794af6
Stefan <stefan_armand@yahoo.com>
2018-12-17 15:45:53 +0200

    Create a class Country, State, City and Village and arrange them in hierarchical manner.

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,8 +1,47 @@
+//Create a class Country, State, City and Village and arrange them in hierarchical manner.
+
 #include "pch.h"
 #include <iostream>
 
 using namespace std;
-int main()
+class Country
 {
-	
+public:
+	void country_show()
+	{
+		cout << "Hello from Country" << endl;
+	}
+};
+class State :public Country
+{
+public:
+	void state_show()
+	{
+		country_show();
+		cout << "Hello from State" << endl;
+	}
+};
+class City : public State
+{
+public:
+	void city_show()
+	{
+		state_show();
+		cout << "Hello from City" << endl;
+	}
+};
+class Village : public City
+{
+public:
+	void village_show()
+	{
+		city_show();
+		cout << "Hello from Village" << endl;
+	}
+};
+
+void main()
+{
+	Village o1;
+	o1.village_show();
 }
