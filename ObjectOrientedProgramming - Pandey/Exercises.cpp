26205dcdc4a1a78e07e67cf70c9742a0e4c3d297
Stefan <stefan_armand@yahoo.com>
2018-12-17 15:52:53 +0200

    Declare a class Vehicle. From this class derived two_wheeler, three_wheeler and four_wheeler class. Display properties of each type of vehicle using member function of the class.

--- ObjectOrientedProgramming - Pandey/Exercises.cpp
+++ ObjectOrientedProgramming - Pandey/Exercises.cpp
@@ -1,47 +1,44 @@
-//Create a class Country, State, City and Village and arrange them in hierarchical manner.
+//Declare a class Vehicle. From this class derived two_wheeler, three_wheeler and four_wheeler class.Display properties of each type of vehicle using member function of the class.
 
 #include "pch.h"
 #include <iostream>
 
 using namespace std;
-class Country
+class Vehicle
+{
+
+};
+class two_wheeler :public Vehicle
 {
 public:
-	void country_show()
+	void twowheel_show()
 	{
-		cout << "Hello from Country" << endl;
+		cout << "This is a two wheeler" << endl;
 	}
 };
-class State :public Country
+class three_wheeler : public Vehicle
 {
 public:
-	void state_show()
+	void threewheel_show()
 	{
-		country_show();
-		cout << "Hello from State" << endl;
+		cout << "This is a three wheeler" << endl;
 	}
 };
-class City : public State
+class four_wheeler : public Vehicle
 {
 public:
-	void city_show()
+	void fourwheel_show()
 	{
-		state_show();
-		cout << "Hello from City" << endl;
-	}
-};
-class Village : public City
-{
-public:
-	void village_show()
-	{
-		city_show();
-		cout << "Hello from Village" << endl;
+		cout << "This is a four wheeler" << endl;
 	}
 };
 
 void main()
 {
-	Village o1;
-	o1.village_show();
+	two_wheeler o1;
+	o1.twowheel_show();
+	three_wheeler o2;
+	o2.threewheel_show();
+	four_wheeler o3;
+	o3.fourwheel_show();
 }
